eb8ef08cb6e78de54688dcc0355f4f69fdf2e84f
pogge6 <dlicudi@gmail.com>
2023-11-21 18:23:56 +0100

    Fixes affecting main frame and update frame on load

--- BagWarden.lua
+++ BagWarden.lua
@@ -2,7 +2,11 @@ local itemTexts = {}
 local itemSets
 local MIN_FRAME_WIDTH = 200
 local MIN_FRAME_HEIGHT = 300
+local AceGUI = LibStub("AceGUI-3.0")
 
+
+local isImportFrameVisible = false
+local importFrame = nil -- This will hold the reference to the import frame
 
 BagWardenFrame = CreateFrame("Frame", "BagWardenFrame", UIParent, BackdropTemplateMixin and "BackdropTemplate")
 FrameTitle = BagWardenFrame:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
@@ -11,7 +15,6 @@ FrameTitle:SetTextColor(1, 1, 0)  -- Yellow color
 
 local currentSetLabel = BagWardenFrame:CreateFontString(nil, "ARTWORK", "GameFontNormal")
 currentSetLabel:SetPoint("TOPLEFT", 5, -5)
-
 
 BagWardenFrame:SetSize(200, 200)
 BagWardenFrame:SetBackdrop({
@@ -31,17 +34,46 @@ BagWardenFrame:SetScript("OnDragStop", BagWardenFrame.StopMovingOrSizing)
 BagWardenFrame:SetScript("OnHide", BagWardenFrame.StopMovingOrSizing)
 
 
-local function GetItemLinkFromName(itemName)
-    local itemID = select(1, GetItemInfoInstant(itemName))
-    if itemID then
-        return select(2, GetItemInfo(itemID))
+
+local function ClearFrameItems()
+    for _, itemText in ipairs(itemTexts) do
+        itemText:Hide()
     end
-    return nil
+    itemTexts = {}
 end
+
+
+
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("GET_ITEM_INFO_RECEIVED")
+
+local function GetItemLinkFromName(itemName, callback)
+    local itemID = select(1, GetItemInfoInstant(itemName))
+    if not itemID then
+        return nil
+    end
+
+    local itemLink = select(2, GetItemInfo(itemID))
+    if itemLink then
+        callback(itemLink)
+    else
+        frame:SetScript("OnEvent", function(self, event, arg1)
+            if event == "GET_ITEM_INFO_RECEIVED" and arg1 == itemID then
+                itemLink = select(2, GetItemInfo(itemID))
+                if itemLink then
+                    callback(itemLink)
+                end
+                self:SetScript("OnEvent", nil)
+            end
+        end)
+    end
+end
+
+
 
 local function CheckItems()
     local maxWidth = 0
-
+    
     if not BagWardenData then
         BagWardenData = DeepCopy(DefaultData)
     end
@@ -52,26 +84,28 @@ local function CheckItems()
     -- Update each item
     for i, item in ipairs(set) do
         local itemName, minCount = item.name, item.minCount
-        local itemLink = GetItemLinkFromName(itemName) -- Get the item link
         local count = GetItemCount(itemName)
         local itemText = itemTexts[i]
-        
+
         if not itemText then
-            -- Create new font string for this item
             itemText = BagWardenFrame:CreateFontString(nil, "ARTWORK", "GameFontNormal")
             itemText:SetText(itemName)
             itemText:SetPoint("TOPLEFT", 5, -5 - (i - 1) * 15 - 30) 
             itemTexts[i] = itemText
         end
 
-        -- Attach tooltip to the itemText
-        itemText:SetScript("OnEnter", function(self)
-            if itemLink then
-                GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
-                GameTooltip:SetHyperlink(itemLink)
-                GameTooltip:Show()
-            end
-        end)
+        local updateTooltip = function(itemLink)
+            itemText:SetScript("OnEnter", function(self)
+                if itemLink then
+                    GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+                    GameTooltip:SetHyperlink(itemLink)
+                    GameTooltip:Show()
+                end
+            end)
+        end
+
+        GetItemLinkFromName(itemName, updateTooltip)
+
     
         itemText:SetScript("OnLeave", function()
             GameTooltip:Hide()
@@ -99,6 +133,54 @@ local function CheckItems()
 end
 
 
+function CreateImportFrame()
+    ImportFrame = AceGUI:Create("Frame")
+    ImportFrame:SetTitle("Update Items")
+    ImportFrame:SetStatusText("Enter items and minimum count, one per line.")
+    ImportFrame:SetLayout("List") -- Change to a List layout to stack widgets vertically
+    ImportFrame:SetWidth(500)
+    ImportFrame:SetHeight(400)
+
+    editbox = AceGUI:Create("MultiLineEditBox")
+    editbox:SetLabel("Item List (Format: Item Name,Min Count)")
+    editbox:DisableButton(true)
+    editbox:SetFullWidth(true)
+    editbox:SetNumLines(20)
+
+    local itemData = ""
+    local currentSetTable = BagWardenData.itemSets[BagWardenData.currentSet]
+    for _, item in ipairs(currentSetTable) do
+        itemData = itemData .. item.name .. "," .. item.minCount .. "\r\n"
+    end
+    editbox:SetText(itemData)
+    ImportFrame:AddChild(editbox)
+
+    local function ImportItems()    
+        local itemsText = editbox:GetText()
+        local currentSetTable = BagWardenData.itemSets[BagWardenData.currentSet]
+
+        for i in pairs(currentSetTable) do
+            currentSetTable[i] = nil
+        end
+
+        for line in itemsText:gmatch("[^\r\n]+") do
+            local itemName, minCount = line:match("^(.-),(%d+)$")
+            minCount = tonumber(minCount)
+            table.insert(currentSetTable, { name = itemName, minCount = minCount })
+            print("Added " .. minCount .. " " .. itemName .. " to the current set.")
+        end
+        CheckItems()
+    end
+
+    -- Create an import button
+    local importBtn = AceGUI:Create("Button")
+    importBtn:SetText("Save")
+    importBtn:SetFullWidth(true)
+    importBtn:SetCallback("OnClick", function() ImportItems() end)
+    ImportFrame:AddChild(importBtn)
+
+end
+
 BagWardenFrame:RegisterEvent("BAG_UPDATE")
 BagWardenFrame:SetScript("OnEvent", CheckItems)
 BagWardenFrame:RegisterEvent("ADDON_LOADED")
@@ -108,7 +190,14 @@ BagWardenFrame:SetScript("OnEvent", function(_, event, arg1)
             BagWardenData = DeepCopy(DefaultData)
         end
 
+        if BagWardenData.hidden ~= true then
+            BagWardenFrame:Show()
+        else
+            BagWardenFrame:Hide()
+        end
         CheckItems()
+        CreateImportFrame()
+        ImportFrame:Hide()
 
         BagWardenFrame:UnregisterEvent("ADDON_LOADED")
     end
@@ -119,14 +208,38 @@ BagWardenFrame:SetScript("OnEvent", function(_, event, arg1)
 end)
 
 
-SLASH_BagWarden1 = "/bw"
+
+-- Create the toggle button
+local toggleButton = CreateFrame("Button", nil, BagWardenFrame, "UIPanelButtonTemplate")
+toggleButton:SetPoint("TOPRIGHT", BagWardenFrame, "TOPRIGHT", -10, -10) -- Right align at 10 pixels from the right edge
+toggleButton:SetSize(60, 22)
+toggleButton:SetText('Update')
+toggleButton:SetScript("OnClick", function()
+
+    if ImportFrame:IsShown() then
+        ImportFrame:Hide()
+    else
+        local itemData = ""
+        local currentSetTable = BagWardenData.itemSets[BagWardenData.currentSet]
+        for _, item in ipairs(currentSetTable) do
+            itemData = itemData .. item.name .. "," .. item.minCount .. "\r\n"
+        end
+        editbox:SetText(itemData)    
+        ImportFrame:Show()
+    end
+
+end)
+
+
+
+SLASH_BagWarden1 = "/bgw"
 SlashCmdList["BagWarden"] = function(msg)
     local cmd, rest = msg:match("^(%S*)%s*(.-)$")
 
     if cmd == "add" then
         local item_name, min_count = rest:match("(.*)%s(%d+)$")
         if item_name then item_name = item_name:match("^%s*(.-)%s*$") end
-        min_count = tonumber(min_count) or 1 -- default to 1 if no minimum count was provided
+        min_count = tonumber(min_count) or 1
         if item_name then
             local currentSetTable = BagWardenData.itemSets[BagWardenData.currentSet]
             for i, item in ipairs(currentSetTable) do
@@ -143,7 +256,7 @@ SlashCmdList["BagWarden"] = function(msg)
         end
     elseif cmd == "remove" then
         local item_name = rest:match("^(.+)$")
-        if item_name then 
+        if item_name then
             item_name = item_name:match("^%s*(.-)%s*$") 
         end
         local currentSetTable = BagWardenData.itemSets[BagWardenData.currentSet]
@@ -153,7 +266,7 @@ SlashCmdList["BagWarden"] = function(msg)
                 break  -- assuming each item name is unique within a set
             end
         end
-        
+
         -- Clear all previous item texts and update the frame with the new list.
         for _, itemText in ipairs(itemTexts) do
             itemText:Hide()
@@ -165,6 +278,7 @@ SlashCmdList["BagWarden"] = function(msg)
     elseif cmd == "load" then
         local setName = rest:match("^(.+)$")
         if setName then
+            ClearFrameItems()  -- Clear the frame before loading a new set
             setName = setName:match("^%s*(.-)%s*$") -- Remove leading and trailing spaces
             if setName then
                 BagWardenData.currentSet = setName
@@ -193,6 +307,7 @@ SlashCmdList["BagWarden"] = function(msg)
             print("Created a new set: " .. setName)
             CheckItems()
         end
+        CheckItems()        
     elseif cmd == "list" then
         print("Saved sets:")
         for setName, set in pairs(BagWardenData.itemSets) do
@@ -204,26 +319,33 @@ SlashCmdList["BagWarden"] = function(msg)
         if BagWardenFrame:IsShown() then
             CheckItems()
         end
+
     elseif cmd == "" then
         if BagWardenFrame:IsShown() then
             BagWardenFrame:Hide()
+            BagWardenData.hidden = true
+
         else
             BagWardenFrame:Show()
+            BagWardenData.hidden = false
             CheckItems()
         end
+
     elseif cmd == "help" then
         print("BagWarden commands:")
-        print("/bw: Toggle visibility of the item check list.")
-        print("/bw createset <set_name>: Create a new set with the given name.")
-        print("/bw removeset <set_name>: Remove a set by its name.")        
-        print("/bw add <item_name> <min_count>: Add an item to the check list. Minimum count is optional.")
-        print("/bw remove <item_name>: Remove an item from the check list.")
-        print("/bw load <set_name>: Load a set.")
-        print("/bw list: List sets.")
-        print("/bw reset: Reset the list to default items.")        
-        print("/bw help: Show this help text.")
+        print("/bgw: Toggle visibility of the item check list.")
+        print("/bgw createset <set_name>: Create a new set with the given name.")
+        print("/bgw removeset <set_name>: Remove a set by its name.")        
+        print("/bgw add <item_name> <min_count>: Add an item to the check list. Minimum count is optional.")
+        print("/bgw remove <item_name>: Remove an item from the check list.")
+        print("/bgw load <set_name>: Load a set.")
+        print("/bgw list: List sets.")
+        print("/bgw reset: Reset the list to default items.")        
+        print("/bgw help: Show this help text.")
     else
-        print("Invalid command. Use: /bw, /bw toggle, /bw add <item_name> <min_count>, /bw remove <item_name>, /bw save <set_name>, /bw load <set_name>, /bw list, /bw reset, or /bw help")
+        print("Invalid command")
     end
 end
+
+
 
--- BagWardenSets.lua
+++ BagWardenSets.lua
@@ -3,116 +3,69 @@ DefaultData = {
     itemSets = {
         ["Default"] = {},
         
-        -- Druid
-        ["Preset:Classic:Druid:Healer"] = {
-            { name = "Flask of Distilled Wisdom", minCount = 1 },
-            { name = "Heavy Runecloth Bandage", minCount = 20 },
-            { name = "Rumsey Rum Black Label", minCount = 5 },
+        -- Naxxramas
+        ["Naxxramas"] = {
+            { name = "Major Healing Potion", minCount = 10 },
             { name = "Major Mana Potion", minCount = 10 },
-            { name = "Mageblood Potion", minCount = 5 },
-            { name = "Demonic Rune", minCount = 5 },
-            { name = "Nightfin Soup", minCount = 20 },
-            { name = "Night Dragon's Breath", minCount = 5 },
+            { name = "Greater Frost Protection Potion", minCount = 5 },
+            { name = "Greater Shadow Protection Potion", minCount = 5 },
             { name = "Greater Fire Protection Potion", minCount = 5 },
             { name = "Greater Nature Protection Potion", minCount = 5 },
-            -- ... other Resto Druid specific items ...
+            { name = "Elixir of Fortitude", minCount = 5 },
+            { name = "Elixir of Greater Defense", minCount = 5 },
+            { name = "Smoked Desert Dumplings", minCount = 20 },
+            { name = "Flask of the Titans", minCount = 1 },
+            -- ... other Naxxramas specific items ...
         },
 
-        -- Mage
-        ["Preset:Classic:Mage"] = {
-            { name = "Flask of Supreme Power", minCount = 1 },
-            { name = "Mana Gem", minCount = 3 },
+        -- AQ40
+        ["AQ40"] = {
+            { name = "Greater Nature Protection Potion", minCount = 5 },
+            { name = "Major Healing Potion", minCount = 10 },
             { name = "Major Mana Potion", minCount = 10 },
-            { name = "Elixir of Greater Intellect", minCount = 5 },
-            { name = "Elixir of Greater Firepower", minCount = 5 },
-            { name = "Brilliant Wizard Oil", minCount = 1 },
-            { name = "Conjured Crystal Water", minCount = 20 },
-            { name = "Conjured Croissant", minCount = 20 },
-            { name = "Greater Arcane Elixir", minCount = 5 },
-            { name = "Arcane Elixir", minCount = 5 },
-            { name = "Elixir of Frost Power", minCount = 5 },
-            -- ... other Mage specific items ...
-        },
-
-        -- Warrior
-        ["Preset:Classic:Warrior:DPS"] = {
-            { name = "Mighty Rage Potion", minCount = 5 },
+            { name = "Elixir of Greater Agility", minCount = 5 },
             { name = "Elixir of the Mongoose", minCount = 5 },
-            { name = "Juju Power", minCount = 5 },
-            { name = "Major Healing Potion", minCount = 5 },
-            { name = "Winterfall Firewater", minCount = 5 },
-            { name = "Free Action Potion", minCount = 5 },
-            { name = "Greater Stoneshield Potion", minCount = 5 },
-            { name = "Grilled Squid", minCount = 20 },
-            -- ... other Warrior specific items ...
-        },
-
-        -- Paladin
-        ["Preset:Classic:Paladin:Holy"] = {
-            { name = "Major Mana Potion", minCount = 10 },
+            { name = "Elixir of Fortitude", minCount = 5 },
             { name = "Flask of Distilled Wisdom", minCount = 1 },
-            { name = "Blessed Sunfruit", minCount = 10 },
-            { name = "Blessed Sunfruit Juice", minCount = 10 },
-            { name = "Elixir of Superior Defense", minCount = 5 },
-            { name = "Elixir of the Mongoose", minCount = 5 },
-            -- ... other Paladin specific items ...
+            { name = "Cured Ham Steak", minCount = 20 },
+            -- ... other AQ40 specific items ...
         },
 
-        -- Hunter
-        ["Preset:Classic:Hunter"] = {
+        -- Blackwing Lair
+        ["Blackwing Lair"] = {
+            { name = "Greater Fire Protection Potion", minCount = 5 },
+            { name = "Major Healing Potion", minCount = 10 },
+            { name = "Major Mana Potion", minCount = 10 },
             { name = "Elixir of the Mongoose", minCount = 5 },
-            { name = "Juju Power", minCount = 10 },
-            { name = "Major Mana Potion", minCount = 5 },
-            { name = "Grilled Squid", minCount = 20 },
-            { name = "Greater Agility", minCount = 5 },
-            { name = "Rapid Fire", minCount = 5 },
-            { name = "Dense Sharpening Stone", minCount = 20 },
-            -- ... other Hunter specific items ...
-        },
-
-        -- Rogue
-        ["Preset:Classic:Rogue"] = {
-            { name = "Elixir of the Mongoose", minCount = 5 },
-            { name = "Thistle Tea", minCount = 5 },
             { name = "Elixir of Greater Agility", minCount = 5 },
             { name = "Smoked Desert Dumplings", minCount = 20 },
-            { name = "Rumsey Rum Black Label", minCount = 5 },
-            { name = "Instant Poison", minCount = 20 },
-            { name = "Crippling Poison", minCount = 20 },
-            -- ... other Rogue specific items ...
+            { name = "Flask of the Titans", minCount = 1 },
+            -- ... other Blackwing Lair specific items ...
         },
 
-        -- Priest
-        ["Preset:Classic:Priest:Healer"] = {
-            { name = "Major Mana Potion", minCount = 10 },
-            { name = "Flask of Distilled Wisdom", minCount = 1 },
-            { name = "Elixir of Shadow Power", minCount = 5 },
-            { name = "Spirit of Zanza", minCount = 1 },
-            { name = "Mageblood Potion", minCount = 5 },
-            { name = "Nightfin Soup", minCount = 20 },
-            -- ... other Priest specific items ...
-        },
-
-        -- Shaman
-        ["Preset:Classic:Shaman:Healer"] = {
-            { name = "Major Mana Potion", minCount = 10 },
-            { name = "Flask of Distilled Wisdom", minCount = 1 },
-            { name = "Mageblood Potion", minCount = 5 },
-            { name = "Nightfin Soup", minCount = 20 },
-            { name = "Elixir of the Mongoose", minCount = 5 },
+        -- Molten Core
+        ["Molten Core"] = {
             { name = "Greater Fire Protection Potion", minCount = 5 },
-            -- ... other Shaman specific items ...
+            { name = "Major Healing Potion", minCount = 10 },
+            { name = "Major Mana Potion", minCount = 10 },
+            { name = "Elixir of the Mongoose", minCount = 5 },
+            { name = "Elixir of Greater Agility", minCount = 5 },
+            { name = "Elixir of Fortitude", minCount = 5 },
+            { name = "Runn Tum Tuber Surprise", minCount = 20 },
+            { name = "Flask of the Titans", minCount = 1 },
+            -- ... other Molten Core specific items ...
         },
 
-        -- Warlock
-        ["Preset:Classic:Warlock"] = {
+        -- Onyxia
+        ["Onyxia"] = {
+            { name = "Greater Fire Protection Potion", minCount = 5 },
+            { name = "Major Healing Potion", minCount = 10 },
             { name = "Major Mana Potion", minCount = 10 },
-            { name = "Elixir of Shadow Power", minCount = 5 },
-            { name = "Flask of Supreme Power", minCount = 1 },
-            { name = "Demonic Rune", minCount = 5 },
-            { name = "Soul Shard", minCount = 20 },
-            { name = "Nightfin Soup", minCount = 20 },
-            -- ... other Warlock specific items ...
-        }
+            { name = "Elixir of the Mongoose", minCount = 5 },
+            { name = "Elixir of Greater Agility", minCount = 5 },
+            { name = "Dragonbreath Chili", minCount = 5 },
+            { name = "Flask of the Titans", minCount = 1 },
+            -- ... other Onyxia specific items ...
+        },
     }
 }
